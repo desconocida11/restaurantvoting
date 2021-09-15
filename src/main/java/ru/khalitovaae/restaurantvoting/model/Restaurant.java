67a43ab737027bfa3f027f91e7c776bc834b8704
desconocida11 <khalitovaae@yandex.ru>
2021-09-15 17:12:19 +0300

    hibernate L2Cache (ehCache) enabled for restaurants

--- src/main/java/ru/khalitovaae/restaurantvoting/model/Restaurant.java
+++ src/main/java/ru/khalitovaae/restaurantvoting/model/Restaurant.java
@@ -2,6 +2,8 @@ package ru.khalitovaae.restaurantvoting.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import java.util.List;
@@ -9,13 +11,16 @@ import java.util.List;
 @Entity
 @Table(name = "restaurants")
 @NoArgsConstructor
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Restaurant extends AbstractNamedEntity {
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")
     @OrderBy("day DESC")
     @JsonManagedReference
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
     private List<Dish> dishes;
 
+    @SuppressWarnings("CopyConstructorMissesField")
     public Restaurant(Restaurant restaurant) {
         this(restaurant.getId(), restaurant.getName());
     }
