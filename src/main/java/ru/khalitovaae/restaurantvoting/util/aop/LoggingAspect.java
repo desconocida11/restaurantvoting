6249b32c50da92cf1e981710b4a13beace498ec2
desconocida11 <khalitovaae@yandex.ru>
2021-09-19 15:37:24 +0300

    logging aspect fix for different controllers

--- src/main/java/ru/khalitovaae/restaurantvoting/util/aop/LoggingAspect.java
+++ src/main/java/ru/khalitovaae/restaurantvoting/util/aop/LoggingAspect.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ru.khalitovaae.restaurantvoting.AuthorizedUser;
 import ru.khalitovaae.restaurantvoting.util.SecurityUtil;
-import ru.khalitovaae.restaurantvoting.web.RestaurantController;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -20,8 +19,8 @@ public class LoggingAspect {
 
     @Before(value = "execution(public * ru.khalitovaae.restaurantvoting.web.*.*(..))")
     public void beforeRestaurantController(JoinPoint joinPoint) {
-        final Logger log = LoggerFactory.getLogger(RestaurantController.class);
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        final Logger log = LoggerFactory.getLogger(signature.getMethod().getDeclaringClass());
         log.info("method: {}", signature.getName());
         AuthorizedUser authUser = SecurityUtil.safeGet();
         log.info("auth user: {}", authUser != null ? authUser.getId() : "anonymous");
